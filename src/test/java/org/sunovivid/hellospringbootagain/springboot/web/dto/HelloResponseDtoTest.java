957d2a2f8050c5d0ef74603be242e162683bc2de
comseung18 <comseung18@koreatech.ac.kr>
2020-11-28 00:09:12 +0900

    [ch2] Add dto

---
+++ src/test/java/org/sunovivid/hellospringbootagain/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,18 @@
+package org.sunovivid.hellospringbootagain.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬북_기능_테스트() {
+        String name = "test";
+        int amount = 100;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
